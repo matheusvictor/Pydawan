b4bdae7f82004939b66f8238fdd085d91f0356bf
Thiago <40046468+SelfLeaker@users.noreply.github.com>
2019-09-28 21:59:37 -0300

    Atualização do programa : l1a09.py
    Programa finalizado.

--- lista01_pt-BR/l1q09.py
+++ lista01_pt-BR/l1q09.py
@@ -4,3 +4,12 @@
 Elaborar um programa que calcule e apresente o volume de uma caixa retangular.
 Use a fórmula: VOLUME ← COMPRIMENTO * LARGURA * ALTURA.
 '''
+
+def calcular_volume_caixa_retangular(comprimento, largura, altura):
+  return comprimento * largura * altura;
+
+if __name__ == "__main__":
+  
+  # Valores 10, 4 e 4 usados apenas para testar a função.
+  volume = calcular_volume_caixa_retangular(10, 4, 4)
+  print(volume)
