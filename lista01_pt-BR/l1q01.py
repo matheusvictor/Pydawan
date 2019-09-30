bd1385d9a80907b567436d9db9170df0ade5c4b6
Sameer <sameer.jain@rsa.com>
2019-09-30 22:04:24 +0530

    resolves #1

--- lista01_pt-BR/l1q01.py
+++ lista01_pt-BR/l1q01.py
@@ -5,3 +5,10 @@ Escreva um programa em Python que armazene o valor 10 em uma variável A e o val
 A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos fazendo com que o valor que está em A passe para B e vice-versa.
 Ao final, escrever os valores que ficaram armazenados nas variáveis.
 '''
+A = 10
+B = 20
+print('Original A:', A)
+print('Original B:', B)
+A, B = B, A # swapping the values
+print('After Swap A:', A)
+print('After Swap B:', B)
