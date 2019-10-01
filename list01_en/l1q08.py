072ab5962dd5d3440bc50b216f64a1e26f97f3d8
Matheus Victor <matheusvictor.salles@gmail.com>
2019-10-01 08:06:37 -0300

    add list01 (en)

---
+++ list01_en/l1q08.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*- [Define a utilização de alguns caracteres especiais, como ~, ^, etc.]
+
+'''
+Elabore um algoritmo que efetue a leitura de três valores (A, B e C) e apresente como resultado final o quadrado da soma dos três valores lidos.
+'''
