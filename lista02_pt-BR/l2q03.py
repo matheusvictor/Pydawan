08e1097b2282002e90e17ebb20141d80da4aa7b0
Matheus Victor <matheusvictor.salles@gmail.com>
2019-10-05 11:21:02 -0300

    adiciona lista02

---
+++ lista02_pt-BR/l2q03.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*- [Define a utilização de alguns caracteres especiais, como ~, ^, etc.]
+
+'''
+Ler as notas da 1ª e 2ª avaliações de um aluno. Calcular a média aritmética simples e escrever uma mensagem que diga se o aluno foi ou não aprovado. Considere que nota igual ou maior que 6 o aluno é aprovado.
+Escreva também a média calculada.
+'''
---
+++ lista02_pt-BR/l2q09.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*- [Define a utilização de alguns caracteres especiais, como ~, ^, etc.]
+
+'''
+'''
