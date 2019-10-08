f02081201ff538eb09204dc960d42bd0dd58d68b
Matheus Victor <matheusvictor.salles@gmail.com>
2019-10-08 09:12:09 -0300

    atualiza lista02

--- lista02_pt-BR/l2q09.py
+++ lista02_pt-BR/l2q09.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*- [Define a utilização de alguns caracteres especiais, como ~, ^, etc.]
 
 '''
+Ler 3 valores (A, B e C) representando as medidas dos lados de um triângulo e escrever se formam ou não um triângulo.
+OBS: para formar um triângulo, o valor de cada lado deve ser menor que a soma dos outros 2 lados.
 '''
