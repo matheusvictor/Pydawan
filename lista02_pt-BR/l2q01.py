9a482c24a6b8a6eb96615193b97578aba6aa0898
Lalith Chandra <guttikondalalith@hotmail.com>
2019-10-09 12:13:28 +0530

    Soluciona l2q01.py #29
    solução para l2q03.py com comentários

--- lista02_pt-BR/l2q01.py
+++ lista02_pt-BR/l2q01.py
@@ -3,3 +3,15 @@
 '''
 Ler 3 valores (considere que não serão informados valores iguais) e escrever o maior deles.
 '''
+def máxima (valor_1= int(input("insira o primeiro valor")),valor_2= int(input("insira o segundo valor")),
+            valor_3= int(input("insira o terceiro valor"))):#função que recebe a entrada do usuário como parâmetro
+  if (valor_1 >= valor_2) and (valor_1 >= valor_3): #condição para verificar se o valor 1 é maior
+	  maior = valor_1
+
+  elif (valor_2 >= valor_1) and (valor_2 >= valor_3): #condição para verificar se o valor 2 é maior 
+    maior = valor_2 
+  else: #condição para verificar se o valor 3 é maior
+    maior = valor_3 
+
+  print (maior) #imprimir o maior valor
+máxima() #chamada de função
