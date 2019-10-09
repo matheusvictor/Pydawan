c9308e6b140382c3dec896f1b38618407a38d237
Edo <kd.do@linkbynet.com>
2019-10-09 11:28:46 +0700

    english translation v2

--- list02_en/l2q03.py
+++ list02_en/l2q03.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*- [Defines the use of some special characters such as ~, ^, etc.]
 
 '''
-Read the grades from the 1st and 2nd assessments of a student. 
-Calculate the simple arithmetic mean and write a message that tells you whether or not the student has passed. 
+Read the grades (ex: English, Art, Math, PE ..etc ) from the 1st and 2nd assessments of a student. 
+Calculate the simple arithmetic that write a message that tells you whether or not the student has passed. 
 Consider that a grade equal to or greater than 6 is approved.
-Also write the calculated average.
+Also give result in average.
 '''
--- list02_en/l2q07.py
+++ list02_en/l2q07.py
@@ -3,7 +3,7 @@
 '''
 Read two values and print one of the following three messages:
 
-- 'equal numbers', if the numbers are equal;
+- 'Equal numbers', if the numbers are equal;
 - 'First is greater' if the first is bigger than the second; 
 - 'Second bigger' if the second is bigger than the first.
 
