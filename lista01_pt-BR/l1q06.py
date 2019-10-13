288f9301ed42ab67b8a943c44153ee253ad3c98b
João Pedro <betajpsg@gmail.com>
2019-10-13 08:24:35 -0300

    resolve l1q06

--- lista01_pt-BR/l1q06.py
+++ lista01_pt-BR/l1q06.py
@@ -5,19 +5,19 @@ Desenvolva um algoritmo capaz de calcular e apresentar o valor do volume de uma 
 Utilize a fórmula: Volume ← π * Raio^2 * Altura
 '''
 
-//Declaração das variáveis principais do exercício.
+#Declaração das variáveis principais do exercício.
 pi = 3.14;
 raio = 0;
 altura= 0;
 volume = 0;
 
-//espaço reservado ao input de dados do usuário
+#espaço reservado ao input de dados do usuário
 
 raio = float(input("Informe o raio da lata: "))
 altura = float(input("Informe a altura da lata: "))
 
-//calculo do volume a partir da fórmula apresentada no exercício
+#calculo do volume a partir da fórmula apresentada no exercício
 volume = pi * (raio*raio) * altura;
 
-//print do resultado para o usuário
+#print do resultado para o usuário
 print("Volume da lata = ",volume,"l");
