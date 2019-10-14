25028ca468393cb2f65695a4d788d3d78b404292
João Pedro <betajpsg@gmail.com>
2019-10-13 21:23:40 -0300

    resolve l1q06

--- lista01_pt-BR/l1q06.py
+++ lista01_pt-BR/l1q06.py
@@ -6,10 +6,10 @@ Utilize a fórmula: Volume ← π * Raio^2 * Altura
 '''
 
 #Declaração das variáveis principais do exercício.
-pi = 3.14;
-raio = 0;
-altura= 0;
-volume = 0;
+pi = 3.14
+raio = 0
+altura= 0
+volume = 0
 
 #espaço reservado ao input de dados do usuário
 
@@ -17,7 +17,7 @@ raio = float(input("Informe o raio da lata: "))
 altura = float(input("Informe a altura da lata: "))
 
 #calculo do volume a partir da fórmula apresentada no exercício
-volume = pi * (raio*raio) * altura;
+volume = pi * (raio*raio) * altura
 
 #print do resultado para o usuário
-print("Volume da lata = ",volume,"l");
+print("Volume da lata = ",volume,"l")
