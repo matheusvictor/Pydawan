e011be15a64528c75e297ac49b9bc46ab7fdbe19
João Pedro <betajpsg@gmail.com>
2019-10-13 21:20:47 -0300

    Update l2q04.py

--- lista02_pt-BR/l2q04.py
+++ lista02_pt-BR/l2q04.py
@@ -7,20 +7,20 @@ A pessoa poderá votar se tiver 16 anos ou mais.
 '''
 
 #Inicialização das variáveis principais do programa;
-anoAtual = 0;
-anoNascimento = 0; 
+anoAtual = 0
+anoNascimento = 0 
 idade = 0
 
 #inputs dos dados solicitados pelo exercício;
-anoAtual = (int(input("Infome o ano atual: ")));
-anoNascimento = (int(input("Infome o seu ano de nascimento: ")));
+anoAtual = (int(input("Infome o ano atual: ")))
+anoNascimento = (int(input("Infome o seu ano de nascimento: ")))
 
 #calculo da idade;
-idade = (anoAtual-anoNascimento);
+idade = (anoAtual-anoNascimento)
 
 #print da idade do usuário;
-print("Idade: ",idade);
+print("Idade: ",idade)
 
 #uso da expressão condicional que é chamado de operador ternário em algumas outras linguagens, mas é um if else simples
 #e pode ser substituido pela forma convencional de escrita;
-print("Pode votar" if idade>=16 else "Não pode votar");
+print("Pode votar" if idade>=16 else "Não pode votar")
