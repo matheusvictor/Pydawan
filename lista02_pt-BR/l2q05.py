da0194a30fd6be2f06ebc11a7e7a3ef1e42a48f4
João Pedro Santos <betajpsg@gmail.com>
2019-10-14 13:33:24 -0300

    resolve l2q05

--- lista02_pt-BR/l2q05.py
+++ lista02_pt-BR/l2q05.py
@@ -3,3 +3,17 @@
 '''
 Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles.
 '''
+#incializacao de variaveis
+num_1 = 0
+num_2 = 0
+
+#coloquei os input como float para o caso do usuario inserir valores nao inteiros
+num_1 = float(input("Informe o primeiro número: "))
+num_2 = float(input("Informe o segundo número: "))
+
+#condicional ifelse para verificar o maior dos valores
+if num_1>num_2:
+    print("O maior é: ",num_1)
+else:
+    print("O maior é: ",num_2)
+        
