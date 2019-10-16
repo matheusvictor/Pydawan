ef4e51bce37c4114d1b451f867c4e25e3facaacb
amusfq <amusfq@gmail.com>
2019-10-16 11:10:12 +0700

    Add Indonesian translation for problem statement

---
+++ list02_id/l2q07.py
@@ -0,0 +1,8 @@
+# - * - coding: utf-8 - * - [Menentukan penggunakan beberapa karakter khusus, seperti ~, ^, dll.]
+
+'''
+Baca dua nilai dan tampilkan salah satu dari tiga pesan berikut:
+
+- 'angka sama', jika memiliki nilai yang sama;
+- 'Pertama lebih besar' jika nilai yang pertama lebih besar dari yang kedua; 'Kedua lebih besar' jika nilai yang kedua lebih besar dari yang pertama.
+'''
