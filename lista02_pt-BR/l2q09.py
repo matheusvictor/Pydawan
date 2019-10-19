f32599afe537fa9d05b73d78076df6279f83e96d
Rodrigo Cerqueira <rodrigocerqueira@infojr.com.br>
2019-10-19 00:20:17 -0300

    resolve l2q09.py

--- lista02_pt-BR/l2q09.py
+++ lista02_pt-BR/l2q09.py
@@ -4,3 +4,12 @@
 Ler 3 valores (A, B e C) representando as medidas dos lados de um triângulo e escrever se formam ou não um triângulo.
 OBS: para formar um triângulo, o valor de cada lado deve ser menor que a soma dos outros 2 lados.
 '''
+
+a = int(input('Primeiro lado do triângulo: '))
+b = int(input('Segundo lado do triângulo: '))
+c = int(input('Terceiro lado do triângulo: '))
+
+if a>=b+c or b>=c+a or c>=a+b:
+	print('Não formam um triângulo')
+else:
+	print('Formam um triângulo')
