89dd8ddfb2d115a6b2403f3e1012a7e42aa9217f
Lalith Chandra <guttikondalalith@hotmail.com>
2019-10-22 21:58:38 +0530

    updated resolver l1q05
    Changed incorrect comments
    changed input data type from int to float

--- list01_en/l1q05.py
+++ list01_en/l1q05.py
@@ -4,8 +4,8 @@
 Develop an algorithm that can read a temperature in degrees Fahrenheit and display it converted to degrees Celsius.
 The conversion formula is C ← ((F - 32) * 5) / 9, where F is the temperature in Fahrenheit and C is the temperature in Celsius.
 '''
-def tempconv(): #function that converts temperature from celcius to Fahrenheit
-  tempinF = int(input("Enter the temperature in Fahrenheit "))   #takes and stores the temperature in Celsius
+def tempconv(): #function that converts temperature from Fahrenheit to celcius 
+  tempinF = float(input("Enter the temperature in Fahrenheit "))   #takes and stores the temperature in Fahrenheit
   tempinC = ((tempinF - 32) * 5) / 9 
-  print ("The temperature {} F in C is {}".format(tempinF,tempinC)) #prints temperature in Celsius
+  print ("The temperature {} F in C is {:.2f}".format(tempinF,tempinC)) #prints temperature in Celsius
 tempconv()
