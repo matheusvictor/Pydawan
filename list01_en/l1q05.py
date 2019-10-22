0d8a76d881f554138f4c02113882ec143694168e
Lalith Chandra <guttikondalalith@hotmail.com>
2019-10-22 14:52:59 +0530

    Resolver l1q05.py #28
    Added Fahrenheit to Celsius converter

--- list01_en/l1q05.py
+++ list01_en/l1q05.py
@@ -4,3 +4,8 @@
 Develop an algorithm that can read a temperature in degrees Fahrenheit and display it converted to degrees Celsius.
 The conversion formula is C ← ((F - 32) * 5) / 9, where F is the temperature in Fahrenheit and C is the temperature in Celsius.
 '''
+def tempconv(): #function that converts temperature from celcius to Fahrenheit
+  tempinF = int(input("Enter the temperature in Fahrenheit "))   #takes and stores the temperature in Celsius
+  tempinC = ((tempinF - 32) * 5) / 9 
+  print ("The temperature {} F in C is {}".format(tempinF,tempinC)) #prints temperature in Celsius
+tempconv()
